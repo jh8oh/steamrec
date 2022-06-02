3053bf844cb9816ecb041436be9e8bab8077620a
jh8oh <ohjiho.ca@gmail.com>
2022-06-02 00:31:26 -0400

    Added checker and logout

--- src/routes/auth-route.js
+++ src/routes/auth-route.js
@@ -3,17 +3,34 @@ import passport from "passport";
 
 const router = express.Router();
 
+router.get("/auth/check", (req, res) => {
+  console.log("User:" + req.user);
+  console.log(req.session.passport);
+  if (req.user === undefined) {
+    res.json({});
+  } else {
+    res.json({
+      user: req.user,
+    });
+  }
+});
+
 router.get(
   "/auth/redirect",
-  passport.authenticate("steam", { failureRedirect: '/login/failed' })
+  passport.authenticate("steam", { failureRedirect: "/" })
 );
 
 router.get(
   "/auth/return",
-  passport.authenticate("steam", {
-    successRedirect: `${process.env.BASE_URL}/`,
-    failureRedirect: "/login/failed",
-  })
+  passport.authenticate("steam", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/");
+  }
 );
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
 
 export default router;
