78d2cf0af2809084bf0fba0436cd1fbb1bea6462
jh8oh <ohjiho.ca@gmail.com>
2022-11-27 12:23:42 +0700

    Removed getting info from steam

--- src/routes/steam-route.js
+++ src/routes/steam-route.js
@@ -35,33 +35,4 @@ router.get("/api/owned-games", (req, res) => {
   });
 });
 
-// Get info about a game
-router.get("/api/game-info/:appid", (req, res) => {
-  if (req.user === undefined) {
-    res.redirect("/");
-    return;
-  }
-
-  const appId = req.params.appid;
-  const url = `${STEAM_STORE_URL}/api/appdetails?`;
-  const query = queryString.stringify({
-    appids: appId,
-  });
-
-  axios.get(url + query).then((response) => {
-    const data = response.data[appId].data;
-
-    res.json({
-      name: data.name,
-      img: data.header_image,
-      type: data.type,
-      desc: data.short_description,
-      genre: data.genres.map(({ description }) => ({ name: description })),
-      categories: data.categories.map(({ description }) => ({
-        name: description,
-      })),
-    });
-  });
-});
-
 export default router;
